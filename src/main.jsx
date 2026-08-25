06f8006d03b7d1991abe808749026050c88d483a
farhana57 <farhanakhatun22@gmail.com>
2026-08-25 18:17:03 +0600

    Fix router basename and update config for custom domain

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/Jahan-_-Traders">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
